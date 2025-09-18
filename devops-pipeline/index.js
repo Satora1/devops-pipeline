8def6bd79d1ca8e13d5cea39fc74e74636a3b93f
Kuba <kubasatora@wp.pl>
2025-09-18 14:46:55 +0200

    first commit

---
+++ devops-pipeline/index.js
@@ -0,0 +1,2 @@
+console.log("Hello, Devopssss");
+console.log("im lerning devops");
---
+++ devops-pipeline/test.js
@@ -0,0 +1,5 @@
+console.log("starting test")
+setTimeout(() => {
+    console.log("test in 3 sec ")
+}, 3000);
+console.log("test finished")
